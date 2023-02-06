5e5e846d583218fb5f9f5fe364edfa70133499dc
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-02-06 23:27:50 +0530

    Completed part - 3 & full functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const year = new Date().getFullYear();
-    return (
-      <footer>
-        <p>Copyright &copy; {year}</p>
-      </footer>
-    );
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
